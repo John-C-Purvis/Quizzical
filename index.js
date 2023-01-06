13f8a421067184f04471f7f600f8c35f2cb46552
John C. Purvis <109570628+John-C-Purvis@users.noreply.github.com>
2023-01-06 15:05:17 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+
+// API link to OTDB:
+// https://opentdb.com/api.php?amount=5
+
+ReactDOM.render(<App />, document.getElementById("root"))
